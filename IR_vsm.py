9d62a20f0600ddb005d4c03a9f82eddb04ef9830
KraftOreo <38533691+KraftOreo@users.noreply.github.com>
2018-10-17 21:56:15 +0800

    the newest version
    the newest version that classify the test data and  output the accuracy

--- IR_vsm.py
+++ IR_vsm.py
@@ -1,11 +1,11 @@
 import os
-import sys
+import math
 import chardet
-import codecs
-from gensim import corpora, models  # similarities
+from gensim import corpora
 from nltk import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
+import random
 
 
 # derive the encoding format of the data
@@ -15,40 +15,16 @@ def get_encoding(file):
         return chardet.detect(f.read())['encoding']
 
 
-# read files and decode them
-def read_and_decode_content(path):
-    new_contents = []
-    files = os.listdir(path)
-    for file in files:
-        with open(path + '\\' + file, 'rb') as f:
-            contents = f.readlines()
-            code_format = get_encoding(path + '\\' + file)
-            if not code_format == None:
-                for content in contents:
-                    content = content.decode(code_format)
-                    new_contents.append(content)
-    return new_contents
-
-
-# If you would like to change the encoding format to utf-8, use the function
-def convert_file_to_utf8(filename):
-    # !!! does not backup the origin file
-    content = codecs.open(filename, 'r').read()
-    source_encoding = chardet.detect(content)['encoding']
-    if source_encoding == None:
-        print("??", filename)
-        return
-    print("  ", source_encoding, filename)
-    if source_encoding != 'utf-8' and source_encoding != 'UTF-8-SIG':
-        content = content.decode(source_encoding, 'ignore')  # .encode(source_encoding)
-        codecs.open(filename, 'w', encoding='UTF-8-SIG').write(content)
-
-
 # build every single species dataset
 def build_classification_dataset(path):
     contents = []
     files = os.listdir(path)
-    for file in files:
+
+    random.shuffle(files)
+    ratio = math.floor(0.7 * len(files))
+    file_train = files[:ratio]
+    file_test = files[ratio:]
+    for file in file_train:
         with open(path + '\\' + file, 'rb') as text:
             all_contents = text.readlines()
             code_format = get_encoding(path + '\\' + file)
@@ -57,8 +33,11 @@ def build_classification_dataset(path):
                     decoded_content = content.decode(code_format)
                     decoded_content = str(decoded_content)
                     contents.append(decoded_content)
-    contents = [' '.join(contents)]
-    return contents
+    contents = ' '.join(contents)
+    test_path = []
+    for file in file_test:
+        test_path.append(path + '\\' + file)
+    return contents, test_path
 
 
 # dataset of every file
@@ -72,41 +51,26 @@ def build_small_dataset(path):
                 decoded_content = content.decode(code_format)
                 decoded_content = str(decoded_content)
                 contents.append(decoded_content)
-
+        contents = ' '.join(contents)
     return contents
-
-
-# clean the data by delete the '\n'
-# I think it's also no use.
-def wash_data(dataset):
-    new_dataset = []
-    for data in dataset:
-        if data[-1] == '\n':
-            data = data[::-1]
-        if data == '\n':
-            del (data)
-            continue
-        new_dataset.append(data)
-
-    return new_dataset
 
 
 # to derive a better and cleaner dataset
 def data_processing(dataset):
     dataset_lower = [data.lower() for data in dataset]
     dataset_tokenized = [word_tokenize(data) for data in dataset_lower]
-    dataset_tokenized = dataset_tokenized[0]
     stop_words = stopwords.words('english')
     dataset_filtered = [words for words in dataset_tokenized if words not in stop_words]
     english_styled_punctuations = [',', '.''?', '/', ':', '<', '>', '[', ']', '(', ')', '*', '&', '^', '%', '$', '#',
                                    '@', '!', '~', '`', '+', '_', '-']
-    dataset_with_no_punctuations = [words for words in dataset_filtered if words not in english_styled_punctuations]
+    dataset_with_no_punctuations = [[words for words in data if words not in english_styled_punctuations] for data in
+                                    dataset_filtered]
     return dataset_with_no_punctuations
 
 
 def data_stemming(dataset):
     ps = PorterStemmer()
-    dataset_stemmed = [ps.stem(word) for word in dataset]
+    dataset_stemmed = [[ps.stem(word) for word in data] for data in dataset]
     return dataset_stemmed
 
 
@@ -119,24 +83,51 @@ def build_corpus(dataset):
 def build_vs_model(dataset):
     data_processed = data_processing(dataset)
     dataset_stemmed = data_stemming(data_processed)
-    # print(dataset_stemmed)
-    corpus, dictionary = build_corpus([dataset_stemmed])
-    tfidf_model = models.TfidfModel(corpus)
-    corpus_tfidf = [tfidf_model[doc] for doc in corpus]
-    return corpus_tfidf, dictionary, corpus
-
-
-def tfidf_model_small(dataset, dir):
-    corpus_tfidf, dictionary, corpus = build_vs_model(dataset)
-    return {'corpus_tfidf_small' + dir: corpus_tfidf, 'dictionary_small' + dir: dictionary,
-            'corpus_small' + dir: corpus}
+    corpus, dictionary = build_corpus(dataset_stemmed)
+    return dictionary, corpus
 
 
 if __name__ == '__main__':
     path = "C:\\Users\\97653\\Desktop\\人工智能\\IR&DM\\20news-19997\\20_newsgroups"
+    dict_list = []
+    dataset_overall = []
+    path_to_test = {}
     for dir in os.listdir(path):
         files = os.listdir(path + '\\' + dir)
-        dataset = build_classification_dataset(path + '\\' + dir)
-        dictionary_name='dictionary'+dir
-        corpus_name = 'corpus' + dir
-        corpus_tfidf_none, dictionary_name, corpus_name = build_vs_model(dataset)
+        dataset, test_path = build_classification_dataset(path + '\\' + dir)
+        dataset_overall.append(dataset)
+        path_to_test[dir] = test_path
+    dictionary, corpus = build_vs_model(dataset_overall)
+
+    prob = math.log(1 / 20)
+    true = 0
+    false = 0
+    for dirs in path_to_test.keys():
+        i = list(path_to_test.keys()).index(dirs)
+        print(i)
+        for test_sample in path_to_test[dirs]:
+            sample_data = build_small_dataset(test_sample)
+            sample_dictionary, sample_corpus = build_vs_model(sample_data)
+            for key in sample_dictionary.token2id.keys():
+                if key in dictionary.token2id.keys():
+                    idx = dictionary.token2id[key]
+                    prob_list = []
+                    for corpus_class in corpus:
+                        SUM = 0
+                        SUM_imagine = len(corpus_class)
+                        p = 0
+                        for tuples in corpus_class:
+                            SUM += tuples[1]
+                        for tuples in corpus_class:
+                            if tuples[0] == idx:
+                                p += math.log(tuples[1] + 1 / SUM + SUM_imagine)
+                            else:
+                                p += math.log(1 / SUM + SUM_imagine)
+                        prob_list.append(p)
+                    print(prob_list.index(max(prob_list)))
+                    a = prob_list.index(max(prob_list))
+                    if i == a:
+                        true += 1
+                    else:
+                        false += 1
+    print(true / (true + false))
